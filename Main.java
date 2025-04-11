b3327b91bc18bdcf9b57ed374a0e4abf1b68e9a6
Hamza <hamzabenbrahim6060@gmail.com>
2025-04-11 14:09:24 +0100

    updating code 02

--- Main.java
+++ Main.java
@@ -2,11 +2,13 @@ public class Main {
     public static void main(String[] args) {
         System.out.println(500);
         System.out.println("dima wydad");
+        System.out.println("lulu carrying mehdi");
 
     }
     public static boolean mehdiKe3ku3 (){
         return true;
         }
+
 
     }
 
